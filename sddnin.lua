9fdb92f07fe5c82f8007f1d0ea9851192257181a
Sddnin1 <sunblox1k9@gmail.com>
2025-03-31 11:34:09 +0700

    Update sddnin.lua

--- sddnin.lua
+++ sddnin.lua
@@ -117,3 +117,26 @@ AddButton(Tab1o, {
 loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
 		end
 	})
+
+AddButton(Tab1o, {
+     Name = "Min gaming",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+			}
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinXoE"))()
+		end
+	}) 
+
+
+AddButton(Tab1o, {
+     Name = "Teddy Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+			}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditoiletgojo/Haidepzai/refs/heads/main/Teddy-FREMIUM"))()
+		end
+	}) 
